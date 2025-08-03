3f88ba5bdeb32ae7c39551485979a8baf9ca859f
Simon spier <jacquessimon656@gmail.com>
2025-08-04 04:32:50 +0530

    save caart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,18 @@
 import {products} from "./products.js";
 
- export let cart=[{
+ export let cart=JSON.parse(localStorage.getItem('cart'))
+  ||
+ [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
  },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
  }];
+
+ function SavetoStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+ }
 
  export function AddToCart(productId)
  {
@@ -25,6 +31,7 @@ import {products} from "./products.js";
        quantity:1
      });
      }
+     SavetoStorage();
  }
 
 
@@ -37,5 +44,6 @@ import {products} from "./products.js";
     }
   });
   cart=NewCart;
+  SavetoStorage();
  }
  
