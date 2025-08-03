e769b5ef33c7b58c744f2f6b74f17f636bca73c1
Simon spier <jacquessimon656@gmail.com>
2025-08-04 04:20:51 +0530

    delete products from html when clincking delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((CartItem) => {
   });
   CartSummaryHTML+=
   `
-<div class="cart-item-container">
+<div class="cart-item-container 
+js-cart-item-container-${matchingproduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -99,6 +100,8 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click',()=>{
     const productId=link.dataset.productId;
     removefromCart(productId);
+    let container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
     
 
   })
