7bfeb2bd1437bdfed0861c8288f3fd8852fe4a5a
Simon spier <jacquessimon656@gmail.com>
2025-09-04 08:50:41 +0530

    add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummery } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/PaymentSummary.js";
 // import '../data/cart-class.js';
+import '../data/backend-practice.js';
 renderOrderSummery();
 renderPaymentSummary();
